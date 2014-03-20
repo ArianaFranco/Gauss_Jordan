93cfb3a8b975e066720bd8f0b7fabb3de3409e88
ArianaFranco <arifraflo@hotmail.com>
2014-03-20 12:33:36 -0600

    Update gauss.erl

--- src/gauss.erl
+++ src/gauss.erl
@@ -1,10 +1,8 @@
 -module(gauss).
 -export([main/1]).
--import(tam_gauss,[tam/1]).
--import(inicia_gauss,[gauss/2]).
 main(_,false)->
 io:format("Error~n");
 main(M,Tam)->
-gauss(M,Tam).
+inicia_gauss:gauss(M,Tam).
 main(Matriz)->
-main(Matriz,tam(Matriz)).
+main(Matriz,tam_gauss:tam(Matriz)).
